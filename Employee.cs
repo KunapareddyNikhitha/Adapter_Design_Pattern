2bc5e599e5921be61f7072c200aa583e86f8c132
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-04 10:36:34 +0530

    Adapter Design pattern

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Design_Pattern
+{
+    public class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public Employee(string id, string name, string designation, decimal salary)
+        {
+            ID = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Adapter_Design_Pattern
+{
+    //Client 
+    //The Client is Incompatible with ThirdPartyBillingSystem 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Storing the Employees Data in a String Array
+            string[,] employeesArray = new string[5, 4]
+            {
+                {"SS221","Nikhitha","SE","10000"},
+                {"SS222","RajKiran","SE","200000"},
+                {"SS223","Subash","SSE","30000"},
+                {"SS224","Dev","SE","40000"},
+                {"SS225","Hansika","SSE","50000"}
+            };
+
+            //The EmployeeAdapter Makes it possible to work with Two Incompatible Interfaces
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            ITarget target = new EmployeeAdapter();
+            target.ProcessCompanySalary(employeesArray);
+
+            Console.Read();
+        }
+    }
+}
